c32bfbe53bd25c4f4edf7655c7b88806fccef224
Marukyan Albert <albertmarukyan@yandex.ru>
2023-06-06 17:09:45 +0300

    Feat: Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,33 +1,37 @@
 import logo from "../assets/output-onlinepngtools (8).png";
+import avatar from "../assets/DCA6ABF9-EE3C-4E5C-84DD-455071481FB3_1_102_o.jpeg";
+import { FaSearch, FaPlus } from "react-icons/fa";
 
 function Navbar() {
   return (
     <div
       className="navbar
- bg-[#1b1d1f] flex gap-2 w-100 justify-between align-top px-4 py-6 text-center"
+ bg-[#1b1d1f] flex gap-2 w-100 justify-between align-top px-[5rem] py-6 text-center"
     >
       <div className="w-[33%]">
-        <span
-          class="flex items-center whitespace-nowrap rounded-l border border-r-0 border-solid border-neutral-300 px-3 py-[0.25rem] text-center text-base font-normal leading-[1.6] text-neutral-700"
-          id="basic-addon1"
-        >
-          reac
-        </span>
-        <input
-          type="text"
-          className="rounded-full w-[13rem] text-lg py-1 bg-[#353535] text-white"
-        />
+        <div className=" flex   rounded-full w-[13rem]  text-lg py-1 bg-[#353535] text-white">
+          <span class="text-center  text-white px-3 py-1">
+            <FaSearch />
+          </span>
+          <input
+            type="text"
+            className="w-[9rem] active:border-none active:outline-none focus:outline-none bg-transparent"
+          />
+        </div>
       </div>
       <div className="w-[33%] ">
         <img src={logo} className="w-[75px] m-auto" alt="logo" />
-        <h4 className="text-[#908c82] text-xl font-semibold mt-2">
+        <h4 className="text-[#d1cdc5] text-xl font-semibold mt-2">
           Things to focus on today.
         </h4>
       </div>
-      <div className="w-[33%] ">
-        <div className="btn btn-primary rounded ">Create</div>
-        <div className="rounded">
-          <img src="" alt="Avatar" /> Avatar
+      <div className="w-[33%]  ">
+        <div className="flex gap-5 w-[100%] justify-end">
+          <div className="btn btn-primary rounded-full border-[#38dbe0]  px-4 text-[#38dbe0] border-solid border-2 flex gap-2 py-2 ">
+            <FaPlus className="my-1.5" />
+            <p className="text-lg font-[900]">CREATE</p>
+          </div>
+          <img src={avatar} className="rounded-full w-[50px]" alt="Avatar" />{" "}
         </div>
       </div>
     </div>
