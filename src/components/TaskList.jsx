6fa207542fa8a81585b9e0c69a8c5a675f6d351a
Marukyan Albert <albertmarukyan@yandex.ru>
2023-06-09 02:22:25 +0300

    Fix: Tasks uniting

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,5 +1,4 @@
 import Task from "./blocks/Task";
-import TaskBlack from "./blocks/TaskBlack";
 import { useEffect, useState } from "react";
 import {
   collection,
@@ -13,7 +12,7 @@ import { db } from "../firebase.config";
 
 function TaskList({ isDone }) {
   const [tasks, setTasks] = useState();
-  const [done, setDone] = useState(false);
+  const [doneInf, setDoneInf] = useState(false);
   useEffect(() => {
     const getTasks = async () => {
       try {
@@ -37,24 +36,20 @@ function TaskList({ isDone }) {
       }
     };
     getTasks();
-  }, []);
+  }, [doneInf]);
 
   return (
     <main className="flex  flex-col gap-6 px-[5rem]">
       {tasks &&
-        tasks.map((el, index) =>
-          index === 0 ? (
-            <Task
-              task={el.data}
-              done={done}
-              setDone={setDone}
-              id={el.id}
-              key={index}
-            />
-          ) : (
-            <TaskBlack task={el.data} id={el.id} key={index} />
-          )
-        )}
+        tasks.map((el, index) => (
+          <Task
+            task={el.data}
+            setDoneInf={setDoneInf}
+            id={el.id}
+            key={index}
+            isDark={index !== 0}
+          />
+        ))}
     </main>
   );
 }
--- src/components/blocks/Task.jsx
+++ src/components/blocks/Task.jsx
@@ -3,40 +3,80 @@ import { updateDoc, doc, collection } from "firebase/firestore";
 import { db } from "../../firebase.config";
 import { useState } from "react";
 
-function Task({ task, id, done, setDone }) {
+function Task({ task, id, setDoneInf, isDark }) {
+  const [done, setDone] = useState(task.done);
   const editTask = async (doneD) => {
+    console.log(doneD, task);
+    setDoneInf(Math.random());
     setDone(doneD);
     await updateDoc(doc(db, "tasks", id), { ...task, done: doneD });
   };
   return (
-    <div className="w-[100%] bg-[#38dbe0] text-black rounded-2xl shadow-xl shadow-[#00000047]">
-      <div className="bg-[#2fc7cd] rounded-t-xl flex justify-between w-[100%] py-3 px-7">
+    <div
+      className={`w-[100%] ${
+        isDark ? "bg-[#242527]" : "bg-[#38dbe0] text-black"
+      } rounded-2xl shadow-xl shadow-[#00000047]`}
+    >
+      <div
+        className={`${
+          isDark ? " bg-[#383a39]" : "bg-[#2fc7cd] "
+        } rounded-t-xl flex justify-between w-[100%] py-3 px-7`}
+      >
         <div className="ml-[3.5rem]">
-          <h4 className="font-bold  text-uppercase text-xl">{task.category}</h4>
-          <h2 className="font-bold  text-uppercase text-2xl ">
+          <h4
+            className={`font-bold  text-uppercase text-xl ${
+              isDark && "text-[#8e897b]"
+            }`}
+          >
+            {task.category}
+          </h4>
+          <h2
+            className={`font-bold  text-uppercase text-2xl  ${
+              isDark && "text-white"
+            }`}
+          >
             {task.project}
           </h2>
         </div>
-        <FaPlus className="my-5 text-xl cursor-pointer" />
+        <FaPlus
+          className={`my-5 text-xl cursor-pointer ${
+            isDark && "text-[#8e897b]"
+          }`}
+        />
       </div>
       <div className="px-7 py-5 rounded-b-xl flex justify-between align-middle m-auto">
         <div className="flex gap-3 align-middle">
+          {console.log(task.title, task.done, done)}
           <input
             type="checkbox"
-            className="appearance-none  border-[2.5px] rounded-md m-auto border-black border-solid w-[35px] h-[35px] checked:bg-black"
+            className={`appearance-none  border-[2.5px] rounded-md m-auto border-solid w-[35px] h-[35px]  ${
+              isDark
+                ? "checked:bg-[#38dbe0] border-[#38dbe0] "
+                : "checked:bg-black border-black "
+            }`}
             checked={done}
             onChange={(e) => {
               editTask(e.target.checked);
             }}
           />
           <div className="ml-[.75rem]">
-            <h2 className="font-bold  text-uppercase text-[1.4rem]">
+            <h2
+              className={`font-bold  text-uppercase text-[1.4rem] ${
+                isDark && "text-white"
+              }`}
+            >
               {task.title}
             </h2>
-            <p className="text-md"> {task.desc}</p>
+            <p className={`text-md ${isDark && "text-[#8e897b]"}`}>
+              {task.desc}
+            </p>
           </div>
         </div>
-        <p className="text-lg my-auto font-bold">Tommorow</p>
+        <p
+          className={`text-lg my-auto font-bold ${isDark && "text-[#8e897b]"}`}
+        >
+          Tommorow
+        </p>
       </div>
     </div>
   );
