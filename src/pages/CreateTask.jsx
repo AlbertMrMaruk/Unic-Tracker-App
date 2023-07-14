002e05737aec369b7b70e06edd5193fb7ea8d3ef
Marukyan Albert <albertmarukyan@yandex.ru>
2023-07-15 02:18:23 +0300

    Feat: Adding Instagram Reels

--- src/pages/CreateTask.jsx
+++ src/pages/CreateTask.jsx
@@ -10,24 +10,21 @@ import Navbar from "../components/Navbar";
 import Field from "../components/blocks/Field";
 import {
   collection,
-  //  addDoc,
+  addDoc,
   getDocs,
-  // serverTimestamp,
+  serverTimestamp,
 } from "firebase/firestore";
 import { db } from "../firebase.config";
 import { useEffect, useState } from "react";
-// import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Textarea from "../components/blocks/Textarea";
-import { Reels } from "../services/ReelsService";
 
 function CreateTask() {
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
   const [projects, setProjects] = useState([]);
   const [project, setProject] = useState({});
   const [date, setDate] = useState(new Date());
   const [desc, setDesc] = useState("");
-  const [videos, setVideos] = useState("");
-  const [hidden, setHidden] = useState(true);
   const [task, setTask] = useState("");
   // const [, setPermissionState] = useState(PERMISSION_STATES.UNKNOWN);
   useEffect(() => {
@@ -70,66 +67,24 @@ function CreateTask() {
     //   );
     //   console.log(pushSubscription);
     // }
-    const media = await Reels.requestTopMedia();
-    const list = [];
-    for (const el of media.data.slice(10, 20)) {
-      if (el.media_type === "IMAGE") {
-        list.push({ type: "img", src: el.media_url });
-      } else {
-        const src = await Reels.requestMediaURL(el.permalink);
-        src &&
-          list.push({
-            type: "video",
-            src,
-          });
-      }
-    }
-    console.log(list);
-    setVideos(list);
-    setHidden(false);
+
     //media.data[0].permalink
     //https://www.instagram.com/p/CuPl6mSNng4/
-    // await addDoc(collection(db, "tasks"), {
-    //   title: task,
-    //   project: project.data.name,
-    //   projectId: project.id,
-    //   categoryId: project.data.categoryId,
-    //   category: project.data.category,
-    //   desc: desc,
-    //   createdAt: serverTimestamp(),
-    //   timestamp: serverTimestamp(new Date(date).getTime() / 1000),
-    //   done: false,
-    // });
-    // navigate("/");
+    await addDoc(collection(db, "tasks"), {
+      title: task,
+      project: project.data.name,
+      projectId: project.id,
+      categoryId: project.data.categoryId,
+      category: project.data.category,
+      desc: desc,
+      createdAt: serverTimestamp(),
+      timestamp: serverTimestamp(new Date(date).getTime() / 1000),
+      done: false,
+    });
+    navigate("/");
   };
   return (
     <div className="bg-[#1b1d1f] h-max min-h-[100vh] pb-10">
-      {hidden || (
-        <div
-          className="bg-[#000000b4] w-full h-full absolute z-50"
-          onClick={() => setHidden(true)}
-        >
-          {videos.map((el) =>
-            el.type === "img" ? (
-              <img
-                src={el.src}
-                alt="Instagram Posts"
-                className="mt-10  z-50 m-auto"
-                width={500}
-                height={500}
-              />
-            ) : (
-              <video
-                src={el.src}
-                className="mt-10  z-50 m-auto"
-                width={500}
-                height={500}
-                controls
-              ></video>
-            )
-          )}
-        </div>
-      )}
       <Navbar />
 
       <div className="static rounded-2xl bg-[#2c2e30] pt-6 px-3 pb-10 w-[55%] m-auto mt-7  shadow-xl shadow-[#00000047] ">
--- src/pages/TasksPage.jsx
+++ src/pages/TasksPage.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
 import Navbar from "../components/Navbar";
 import TaskList from "../components/TaskList";
+import { Reels } from "../services/ReelsService";
 // import useFirebaseMessaging from "@useweb/use-firebase-messaging";
 
 function TasksPage() {
   const [doneInf, setDoneInf] = useState({});
+  const [videos, setVideos] = useState("");
+  const [hidden, setHidden] = useState(true);
   //   const firebaseMessaging = useFirebaseMessaging({
   //     onMessage: (message) => {
   //       console.log(`Received foreground message`, message);
@@ -14,11 +17,59 @@ function TasksPage() {
   //   useEffect(() => {
   //     firebaseMessaging.init();
   //   }, []);
+  const loadReels = async () => {
+    const media = await Reels.requestTopMedia();
+    const list = [];
+    for (const el of media.data.slice(10, 20)) {
+      if (el.media_type === "IMAGE") {
+        list.push({ type: "img", src: el.media_url });
+      } else {
+        const src = await Reels.requestMediaURL(el.permalink);
+        src &&
+          list.push({
+            type: "video",
+            src,
+          });
+      }
+    }
+    console.log(list);
+    setVideos(list);
+    setHidden(false);
+  };
   return (
     <div className="bg-[#1b1d1f] h-max min-h-[100vh]  pb-10">
+      {hidden || (
+        <div
+          className="bg-[#000000b4] w-full h-full absolute z-50"
+          onClick={() => setHidden(true)}
+        >
+          {videos.map((el) =>
+            el.type === "img" ? (
+              <img
+                src={el.src}
+                alt="Instagram Posts"
+                className="mt-10  z-50 m-auto"
+                width={500}
+                height={500}
+              />
+            ) : (
+              <video
+                src={el.src}
+                className="mt-10  z-50 m-auto"
+                width={500}
+                height={500}
+                controls
+              ></video>
+            )
+          )}
+        </div>
+      )}
       <Navbar />
       <TaskList isDone={false} doneInf={doneInf} setDoneInf={setDoneInf} />
-      <h2 className="text-4xl my-10 font-bold text-white text-center">
+      <h2
+        className="text-4xl my-10 font-bold text-white text-center"
+        onClick={loadReels}
+      >
         Done Tasks
       </h2>
       <TaskList isDone={true} doneInf={doneInf} setDoneInf={setDoneInf} />
