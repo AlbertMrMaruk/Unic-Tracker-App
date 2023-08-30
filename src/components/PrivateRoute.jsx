264b9b14a7508473cf5b1a4238a0cc70e25ee3e5
Marukyan Albert <albertmarukyan@yandex.ru>
2023-08-30 14:48:40 +0300

    Feat: Editing new component Spinner

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,16 +1,11 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuthStatus } from "../hooks/useAuthStatus";
-import Navbar from "./Navbar";
-import spinner from "../assets/807.gif";
+import Spinner from "./blocks/Spinner";
 
 const PrivateRoute = () => {
   const { loggedIn, checkingStatus } = useAuthStatus();
   if (checkingStatus) {
-    return (
-      <div className="bg-[#1b1d1f] h-max min-h-[100vh] pb-10 shadow-inner py-[20%]">
-        <img className="m-auto w-[128px]" src={spinner} />
-      </div>
-    );
+    return <Spinner />;
   }
   return loggedIn ? <Outlet /> : <Navigate to="/sign-in" />;
 };
